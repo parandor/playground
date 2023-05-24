decef8de15e1fae2e4720ff04ef895927c5540f0
kalashnikovcok1 <kalashnikovcok1@gmail.com>
2023-05-24 17:40:32 -0400

    fix format

--- influx.py
+++ influx.py
@@ -1,24 +1,25 @@
 from influxdb import InfluxDBClient
 
+
 class InfluxDBSender:
     def __init__(self, host, port, username, password, database):
         self.client = InfluxDBClient(host=host, port=port, username=username, password=password)
         self.database = database
-    
+
     def connect(self):
         try:
             self.client.switch_database(self.database)
             print("Connected to InfluxDB database:", self.database)
         except Exception as e:
             print("Failed to connect to InfluxDB:", str(e))
-    
+
     def send_data(self, data):
         try:
             self.client.write_points(data)
             print("Data sent successfully")
         except Exception as e:
             print("Failed to send data to InfluxDB:", str(e))
-    
+
     def close(self):
         self.client.close()
         print("Connection to InfluxDB closed")
@@ -32,13 +33,13 @@ if __name__ == '__main__':
     username = 'admin'
     password = 'admin'
     database = 'influx'
-    
+
     # Create an instance of the InfluxDBSender class
     sender = InfluxDBSender(host, port, username, password, database)
-    
+
     # Connect to the InfluxDB server
     sender.connect()
-    
+
     # Define the data points you want to send
     data = [
         {
@@ -51,9 +52,9 @@ if __name__ == '__main__':
             }
         }
     ]
-    
+
     # Send the data to the InfluxDB server
     sender.send_data(data)
-    
+
     # Close the connection
     sender.close()
