214a83c5deb9e211ea2600b3900a46d8d6d2b2c2
kalashnikovcok1 <kalashnikovcok1@gmail.com>
2023-05-25 22:34:24 -0400

    make distance sensor into class

--- distance.py
+++ distance.py
@@ -1,51 +1,48 @@
 import RPi.GPIO as GPIO
 import time
 
-# GPIO pin numbers
-TRIG_PIN = 22
-ECHO_PIN = 19
 
-def setup():
-    GPIO.setmode(GPIO.BCM)
+class UltrasonicSensor:
+    def __init__(self, trig_pin=22, echo_pin=19):
+        self.trig_pin = trig_pin
+        self.echo_pin = echo_pin
 
-    # Setup GPIO pins
-    GPIO.setup(TRIG_PIN, GPIO.OUT)
-    GPIO.setup(ECHO_PIN, GPIO.IN)
+    def setup(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.trig_pin, GPIO.OUT)
+        GPIO.setup(self.echo_pin, GPIO.IN)
 
-def get_distance():
-    # Send a short pulse to trigger the ultrasonic sensor
-    GPIO.output(TRIG_PIN, GPIO.HIGH)
-    time.sleep(0.00001)
-    GPIO.output(TRIG_PIN, GPIO.LOW)
+    def get_distance(self):
+        GPIO.output(self.trig_pin, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(self.trig_pin, GPIO.LOW)
 
-    # Wait for the echo response
-    pulse_start = time.time()
-    pulse_end = time.time()
-    while GPIO.input(ECHO_PIN) == 0:
         pulse_start = time.time()
-    while GPIO.input(ECHO_PIN) == 1:
         pulse_end = time.time()
+        while GPIO.input(self.echo_pin) == 0:
+            pulse_start = time.time()
+        while GPIO.input(self.echo_pin) == 1:
+            pulse_end = time.time()
 
-    # Calculate the pulse duration and convert it to distance
-    pulse_duration = pulse_end - pulse_start
-    speed_of_sound = 34300  # Speed of sound in cm/s
-    distance = (pulse_duration * speed_of_sound) / 2
+        pulse_duration = pulse_end - pulse_start
+        speed_of_sound = 34300  # Speed of sound in cm/s
+        distance = (pulse_duration * speed_of_sound) / 2
 
-    return distance
+        return distance
 
-def cleanup():
-    GPIO.cleanup()
+    def cleanup(self):
+        GPIO.cleanup()
 
-# Main program
+
 if __name__ == '__main__':
     try:
-        setup()
+        sensor = UltrasonicSensor()
+        sensor.setup()
         while True:
-            distance = get_distance()
+            distance = sensor.get_distance()
             print(f"Distance: {distance:.1f} cm")
             time.sleep(1)
     except KeyboardInterrupt:
         print("User interrupt triggered")
-        pass
     finally:
-        cleanup()
+        sensor.cleanup()
