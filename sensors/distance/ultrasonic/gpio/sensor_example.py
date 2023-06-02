450c7035b3d207716ff608870a4d919a7d8c7fa1
Kal <kalashnikovcok1@gmail.com>
2023-06-02 18:00:13 -0400

    Tested the ultrasonic hc-sr04 sensor with sample code usage

--- sensors/distance/ultrasonic/gpio/sensor_example.py
+++ sensors/distance/ultrasonic/gpio/sensor_example.py
@@ -1,13 +1,20 @@
 from sensors.distance.ultrasonic.gpio.sensor import UltrasonicSensor
+import time
 
 
 #Example usage
-
 trigger_pin = 22
 echo_pin = 19
 sensor_id = "HC-SR04_EM78P153A"
 buf_size = 200
 sensor = UltrasonicSensor(trigger_pin, echo_pin, buf_size, sensor_id)   
 
-sensor.average_distance()    
-buf = sensor.get_distance_buffer()
+try:
+    while True:
+        sensor.average_distance()    
+        distance = sensor.get_average_distance()        
+        if distance is not None:
+            print("Distance: {} cm".format(distance))
+        time.sleep(0.2)
+except KeyboardInterrupt:
+    sensor.cleanup()
